82940f3db5be66761c6121ff7844aa88b183aa15
Andrey Malyshev <malyshev.andrey@Andreys-MacBook-Pro.local>
2025-06-02 14:05:19 +0300

    Add config, metadata.py, .gitignore files

---
+++ memdex/config.py
@@ -0,0 +1,4 @@
+import yaml
+
+with open('config.yaml') as input_file:
+    config = yaml.safe_load(input_file)
---
+++ memdex/metadata.py
@@ -0,0 +1,67 @@
+import pandas as pd
+
+from .clients import VKGroupClient
+from .config import config
+
+
+def _vk_posts2photos_metadata(posts: list[dict]) -> pd.DataFrame:
+    result = pd.DataFrame(posts)
+
+    result = result[['id', 'attachments']].copy()
+    result = result.rename(columns={'id': 'post_id'})
+
+    result = result.explode('attachments')
+
+    result = result[result['attachments'].str.get('type').eq('photo')]
+    result['attachments'] = result['attachments'].str.get('photo')
+
+    result['url'] = result['attachments'].str.get('orig_photo').str.get('url')
+    result['id'] = result['attachments'].str.get('id').astype('str')
+    result['date'] = result['attachments'].str.get('date')
+
+    result = result.drop(columns='attachments')
+
+    return result
+
+
+def _vk_photos2metadata(photos: list[dict]) -> pd.DataFrame:
+    result = pd.DataFrame(photos)
+
+    result['url'] = result['orig_photo'].str.get('url')
+    result = result[['date', 'post_id', 'id', 'url']].copy()
+
+    return result
+
+
+def _get_vk_group_photos_metadata(client: VKGroupClient) -> pd.DataFrame:
+    photos_metadata = _vk_photos2metadata(client.get_wall_photos())
+
+    posts_photos_metadata = _vk_posts2photos_metadata(client.get_posts())
+    posts_photos_metadata = posts_photos_metadata[posts_photos_metadata['url'].notna()]
+    posts_photos_metadata = posts_photos_metadata.rename(columns={'post_id': '_post_id'})
+
+    result = posts_photos_metadata.merge(photos_metadata, how='outer', validate='many_to_one')
+
+    result['post_id'] = result['_post_id'].combine_first(result['post_id'])
+    result = result[result['post_id'].notna()]
+    result = result.drop(columns='_post_id')
+
+    assert result.notna().all().all()
+    assert not result.duplicated(['post_id', 'id']).any()
+
+    result = result.sort_values('id')
+    result['id'] = result.groupby('post_id').cumcount() + 1
+
+    return result
+
+def get_vk_photos_metadata():
+    result = []
+    for group in config['vk']['groups']:
+        client = VKGroupClient(
+            access_token=config['vk']['token'],
+            name=group,
+            api_version=config['vk']['api_version']
+        )
+        result.append(_get_vk_group_photos_metadata(client))
+    result = pd.concat(result)
+    return result
